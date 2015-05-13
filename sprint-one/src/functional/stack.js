954150441162f6bafa49551d1ce5e95878356f6a
Michael Symmes <msymmes@gmail.com>
2015-05-13 18:39:46 -0400

    Implemented basic stack behavior

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,21 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function(){
+    var result = storage[someInstance.size()];
+    delete storage[someInstance.size()];
+    return result;
   };
 
   someInstance.size = function(){
+    var size = 0;
+    for (var key in storage){
+      size++
+    }
+    return size;
   };
 
   return someInstance;
