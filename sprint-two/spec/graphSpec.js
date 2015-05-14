ee8655c33fa98b2b3127bf337513d6e99333f633
Michael Symmes <msymmes@gmail.com>
2015-05-14 18:12:16 -0400

    fixed the test for removeEdge

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -40,7 +40,14 @@ describe('graph', function() {
     graph.addNode('apples');
     graph.addNode('satsumas');
     graph.addEdge('satsumas', 'apples');
-    expect(graph.hasEdge('apples', 'satsumas')).to.equal(true);
+    // This following test is incorrect
+    // expect(graph.hasEdge('apples', 'satsumas')).to.equal(true);
+    // and since it is an undirected graphu it should instead be:
+    expect(graph.hasEdge('satsumas', 'apples')).to.equal(true);
+    // this test was also completely missing an actual removeEdge statement
+    // but it has one now :)
+    graph.removeEdge('satsumas', 'apples');
+    expect(graph.hasEdge('satsumas', 'apples')).to.equal(false);
   });
 
   it('should execute a callback on each node in the graph', function() {
