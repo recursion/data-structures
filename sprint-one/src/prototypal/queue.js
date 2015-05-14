d2e4cd324b3be7250af1ded0c63ba7045b0924bc
Michael Symmes <msymmes@gmail.com>
2015-05-14 09:09:43 -0400

    Fixed ineffecient size behavior

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -3,29 +3,29 @@ var Queue = function() {
   // but try not not reference your old code in writing the new style.
   var someQueue = Object.create(queueMethods);
   someQueue.storage = {};
+  someQueue._size = 0;
   return someQueue;
 };
 
 var queueMethods = {
   enqueue: function(value){
-    this.storage[this.size()] = value;
+    this.storage[this._size] = value;
+    this._size++;
   },
   dequeue: function(){
     var result = this.storage[0];
-    var initialSize = this.size();
     delete this.storage[0];
     for (var key in this.storage) {
       this.storage[key - 1] = this.storage[key];
     }
-    delete this.storage[initialSize - 1];
+    delete this.storage[this._size - 1];
+    if (this._size) {
+      this._size--;
+    }
     return result;
   },
   size: function(){
-    var size = 0;
-    for (var key in this.storage) {
-      size++;
-    }
-    return size;
+    return this._size;
   }
 };
 
