d50d4b455c5a02e87fe57478783a83adf4fab937
Michael Symmes <msymmes@gmail.com>
2015-05-14 09:07:50 -0400

    Changed size behavior to be more effecient

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,6 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
+  someInstance._size = 0;
   someInstance.storage = {};
   _.extend(someInstance, stackMethods);
   return someInstance;
@@ -9,19 +10,19 @@ var Stack = function() {
 
 var stackMethods = {
   push: function(value){
-    this.storage[this.size()] = value;
+    this.storage[this._size] = value;
+    this._size++;
   },
   pop: function(){
-    var result = this.storage[this.size() - 1];
-    delete this.storage[this.size() - 1];
+    var result = this.storage[this._size - 1];
+    delete this.storage[this._size - 1];
+    if (this._size) {
+      this._size--;
+    }
     return result;
   },
   size: function(){
-    var size = 0;
-    for (var key in this.storage) {
-      size++;
-    }
-    return size;
+    return this._size;
   }
 };
 
