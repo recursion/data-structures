62d92a75f7324f3ddfad52f3a2e9b2d531c3ab42
Michael Symmes <msymmes@gmail.com>
2015-05-14 09:13:45 -0400

    Fixed size ineffeciency

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,27 +2,27 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.storage = {};
+  this._size = 0;
 };
 
 Queue.prototype.enqueue = function(value) {
-  this.storage[this.size()] = value;
+  this.storage[this._size] = value;
+  this._size++;
 }
 Queue.prototype.dequeue = function() {
   var result = this.storage[0];
-  var initialSize = this.size();
   delete this.storage[0];
   for (var key in this.storage) {
     this.storage[key - 1] = this.storage[key];
   }
-  delete this.storage[initialSize - 1];
+  delete this.storage[this._size - 1];
+  if (this._size) {
+    this._size--;
+  }
   return result;
 }
 Queue.prototype.size = function() {
-  var size = 0;
-  for (var key in this.storage) {
-    size++;
-  }
-  return size;
+  return this._size;
 }
 
 
