261ca8ce5b7d4f6abfe95ade5cacf8ec971045a9
Michael Symmes <msymmes@gmail.com>
2015-05-14 09:00:29 -0400

    Changed size function to be more effecient

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function(){
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,26 +8,26 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-    storage[someInstance.size()] = value;
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
     var result = storage[0];
-    var initialSize = someInstance.size();
-
+    var initialSize = size;
     delete storage[0];
+
     for (var key in storage) {
       storage[key - 1] = storage[key];
     }
-    delete storage[initialSize - 1];
+    delete storage[size - 1];
+    if (size) {
+      size--;
+    }
     return result;
   };
 
   someInstance.size = function(){
-    var size = 0;
-    for (var key in storage) {
-      size++;
-    }
     return size;
   };
 
