5738201c8db0fcb0d8a8735562b84cb4034ac419
Michael Symmes <msymmes@gmail.com>
2015-05-14 14:12:13 -0400

    implemented removeNode

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,15 +1,30 @@
 
 
 var Graph = function(){
+  this.children = [];
 };
 
 Graph.prototype.addNode = function(node){
+  this.children.push(node);
 };
 
 Graph.prototype.contains = function(node){
+  var contains = false;
+  _.each(this.children, function(currentNode) {
+    if (currentNode === node) {
+      contains = true;
+    }
+  });
+  return contains;
 };
 
 Graph.prototype.removeNode = function(node){
+  _.each(this.children, function(currentNode, index, collection) {
+    if (currentNode === node) {
+      console.log(collection);
+      collection.splice(index, 1);
+    }
+  });
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
