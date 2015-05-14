5054b7d10f9d7b0afbd892f5dd0408d485b6fa5e
Michael Symmes <msymmes@gmail.com>
2015-05-14 09:04:59 -0400

    Made size behavior more effecient

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,6 +2,8 @@ var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
+  someInstance._size = 0;
+
   someInstance.storage = {};
   _.extend(someInstance, queueMethods);
   return someInstance;
@@ -9,24 +11,23 @@ var Queue = function(){
 
 var queueMethods = {
   enqueue: function(value){
-    this.storage[this.size()] = value;
+    this.storage[this._size] = value;
+    this._size++;
   },
   dequeue: function(){
     var result = this.storage[0];
-    var initialSize = this.size();
     delete this.storage[0];
     for (var key in this.storage) {
       this.storage[key - 1] = this.storage[key];
     }
-    delete this.storage[initialSize - 1];
+    delete this.storage[this.size - 1];
+    if (this._size) {
+      this._size--;
+    }
     return result;
   },
   size: function(){
-    var size = 0;
-    for (var key in this.storage) {
-      size++;
-    }
-    return size;
+    return this._size;
   }
 };
 
