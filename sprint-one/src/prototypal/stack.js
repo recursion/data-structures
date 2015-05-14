51048a510b783315a185f0ee27fc4a2077ca9267
Michael Symmes <msymmes@gmail.com>
2015-05-14 09:11:08 -0400

    Corrected ineffecient size behavior

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,24 +3,25 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var someStack = Object.create(stackMethods);
   someStack.storage = {};
+  someStack._size = 0;
   return someStack;
 };
 
 var stackMethods = {
   push: function(value){
-    this.storage[this.size()] = value;
+    this.storage[this._size] = value;
+    this._size++;
   },
   pop: function(){
-    var result = this.storage[this.size() - 1];
-    delete this.storage[this.size() - 1];
+    var result = this.storage[this._size - 1];
+    delete this.storage[this._size - 1];
+    if (this._size) {
+      this._size--;
+    }
     return result;
   },
   size: function(){
-    var size = 0;
-    for (var key in this.storage) {
-      size++;
-    }
-    return size;
+    return this._size;
   }
 };
 
