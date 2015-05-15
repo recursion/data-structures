0969fd53f79be75fefcbe7d1f639d6fd26868892
Michael Symmes <msymmes@gmail.com>
2015-05-15 13:14:19 -0400

    Implemented basic hash table - all tests passing - implementation feels wonky to me.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,19 +1,49 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  this.hashDiv = ']!:![';
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  var hash = k+this.hashDiv+v;
-  this._storage.set(i, [hash]);
+  if (!this._storage.get(i)) {
+    this._storage.set(i, [k, v]);
+  } else {
+    var bucket = this._storage.get(i);
+    var existing = false;
+    // iterate through each value at the current bucket.
+    _.each(bucket, function(val, index) {
+      // check even indexes only
+      if (index % 2 === 0) {
+        // check if the value in the even index matches the
+        // value of the key we are trying to store
+        if (val === k) {
+          bucket[index + 1] = v;
+          existing = true;
+        }
+      }
+    });
+    // if we did not find an existing matching key in the index to overwrite
+    // store the key and value at new indexes.
+    if (!existing) {
+      bucket[bucket.length] = k;
+      bucket[bucket.length] = v;
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(i)) {
-    return this._storage.get(i)[0].split(this.hashDiv)[1];
+    var bucket = this._storage.get(i);
+    var result;
+    _.each(bucket, function(val, index) {
+      if (index % 2 == 0) {
+        if (val === k) {
+          result = bucket[index + 1];
+        }
+      }
+    });
+    return result;
   } else {
     return null;
   }
@@ -29,4 +59,10 @@ HashTable.prototype.remove = function(k){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+    So i feel like... my implementation here is not correct...
+    Its hard to imagine that the _.each() calls here are keeping this thing in constant time...
+    insert:  O(n) ??
+    retrieve: O(n) ??
+    remove: 0(1)
  */
