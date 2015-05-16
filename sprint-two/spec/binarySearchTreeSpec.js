2e5f054bad2cd42ed43cce2c2f8453b7dc63db66
Michael Symmes <msymmes@gmail.com>
2015-05-16 13:38:00 -0400

    Implemented a bogus test to check the breadthFirstLog()

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -36,4 +36,19 @@ describe('binarySearchTree', function() {
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5,2,3]);
   });
+
+// this test doesnt actually work as it will always pass, what it does do
+// is let us mark-1 eyeball the console log entries
+// really I should be using sinon here to verify the console.log calls, but this will do for now
+  it('should run a breadth first log on the entire tree', function() {
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(8);
+    binarySearchTree.breadthFirstLog();
+    expect(1).to.equal(1);
+  });
 });
