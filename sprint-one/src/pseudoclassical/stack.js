59cc14dab0e44e9cee06d306868d4bf9d2980cd5
Michael Symmes <msymmes@gmail.com>
2015-05-16 17:29:55 -0400

    Fixed iterative size function

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,19 +2,20 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.storage = {};
+  this._size= 0;
 };
 Stack.prototype.push = function(value){
   this.storage[this.size()] = value;
+  this._size++;
 };
 Stack.prototype.pop = function(){
   var result = this.storage[this.size() - 1];
   delete this.storage[this.size() - 1];
+  if (this._size) {
+    this._size--;
+  }
   return result;
 };
 Stack.prototype.size = function(){
-  var size = 0;
-  for (var key in this.storage) {
-    size++;
-  }
-  return size;
+  return this._size;
 };
