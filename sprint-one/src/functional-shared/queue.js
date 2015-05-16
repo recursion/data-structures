294dce6c6296413410f8d9b75d9b231430fbfa60
Michael Symmes <msymmes@gmail.com>
2015-05-16 18:27:42 -0400

    Decrease dequeue to O(1)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,7 +3,7 @@ var Queue = function(){
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
   someInstance._size = 0;
-
+  someInstance._next = 0;
   someInstance.storage = {};
   _.extend(someInstance, queueMethods);
   return someInstance;
@@ -11,19 +11,27 @@ var Queue = function(){
 
 var queueMethods = {
   enqueue: function(value){
-    this.storage[this._size] = value;
+    if (this._size === 0) {
+      this._next = 0;
+    }
+
+    if (this._size <= this._next && this._size !== 0) {
+      this.storage[this._next + 1] = value;
+    } else {
+      this.storage[this._size] = value;
+    }
+
     this._size++;
   },
   dequeue: function(){
-    var result = this.storage[0];
-    delete this.storage[0];
-    for (var key in this.storage) {
-      this.storage[key - 1] = this.storage[key];
-    }
-    delete this.storage[this.size - 1];
+    var result = this.storage[this._next];
+    delete this.storage[this._next];
+    this._next++;
+
     if (this._size) {
       this._size--;
     }
+
     return result;
   },
   size: function(){
