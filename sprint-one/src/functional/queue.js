63efa6aaf0e64955ac7d405375cfb44b71defcad
Michael Symmes <msymmes@gmail.com>
2015-05-16 18:26:08 -0400

    Decreased remove to O(1)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,26 +1,33 @@
 var Queue = function(){
   var someInstance = {};
   var size = 0;
-
+  var next = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
-    storage[size] = value;
+
+    if (size === 0) {
+      next = 0;
+    }
+
+    if (size <= next && size !== 0) {
+      storage[next + 1] = value;
+    } else {
+      storage[size] = value;
+    }
+
     size++;
   };
 
   someInstance.dequeue = function(){
-    var result = storage[0];
-    var initialSize = size;
-    delete storage[0];
+    var result = storage[next];
 
-    for (var key in storage) {
-      storage[key - 1] = storage[key];
-    }
-    delete storage[size - 1];
+    delete storage[next];
+    next++;
+
     if (size) {
       size--;
     }
