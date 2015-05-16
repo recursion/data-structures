17d01c66bc325177e6f62ab7443e1a89309eb37e
Michael Symmes <msymmes@gmail.com>
2015-05-16 13:38:16 -0400

    Implemented breadthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -21,6 +21,7 @@ BinarySearchTree.prototype.insert = function(value){
     }
   }
 };
+
 BinarySearchTree.prototype.contains = function(target){
   if (this.value === target) {
     return true;
@@ -32,6 +33,7 @@ BinarySearchTree.prototype.contains = function(target){
     return false;
   }
 };
+
 BinarySearchTree.prototype.depthFirstLog = function(callback){
   callback(this.value);
   if (this.left !== undefined) {
@@ -42,6 +44,36 @@ BinarySearchTree.prototype.depthFirstLog = function(callback){
   }
 };
 
+BinarySearchTree.prototype.breadthFirstLog = function() {
+
+  console.log(this.value);
+  // find children of the current node
+  var findAndLogChildren = function(node) {
+    var childList = [];
+    if (node.left){
+      childList.push(node.left);
+    }
+    if (node.right) {
+      childList.push(node.right);
+    }
+    return childList;
+  };
+
+  var childList = findAndLogChildren(this);
+  while (childList.length > 0) {
+    // add the children of these children to the list
+    var newList = [];
+    _.each(childList, function(child) {
+      console.log(child.value);
+      newList = newList.concat(findAndLogChildren(child));
+    });
+    if (newList.length === 0) {
+      break;
+    }
+    childList = newList;
+  }
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
