634117ea4014731853ddb72ab20ceb955b3ce235
Michael Symmes <msymmes@gmail.com>
2015-05-16 20:02:16 -0400

    Added some spacing

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -10,11 +10,13 @@ Queue.prototype.enqueue = function(value) {
   if (this._size === 0) {
     this._next = 0;
   }
+
   if (this._size <= this._next && this._size !== 0) {
     this.storage[this._next + 1] = value;
   } else {
     this.storage[this._size] = value;
   }
+
   this._size++;
 };
 
